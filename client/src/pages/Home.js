60c124172dbbc613dd2c4b77b133b625e4c6c3bc
Stormfall13 <85984394+Stormfall13@users.noreply.github.com>
2023-05-24 22:16:42 +0500

    first commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import components
+import LatestProducts from '../components/LatestProducts.js';
+import Hero from '../components/Hero.js';
+
+
+const Home = () => {
+  return (
+    <section>
+      <Hero />
+      <LatestProducts />
+    </section>
+  );
+
+};
+
+export default Home;
